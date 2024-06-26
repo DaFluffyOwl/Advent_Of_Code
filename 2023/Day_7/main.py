bddc2974db1f180fab3f8aa4269e692d1567fa03
rpsmith4 <creeper47@outlook.com>
2024-06-25 22:18:36 -0400

    Almost done with Part 1 of this

--- 2023/Day_7/main.py
+++ 2023/Day_7/main.py
@@ -8,7 +8,6 @@ class Cardgame:
             else:
                 self.cards[card] = 1
 
-
     def is5ofKind(self):
         for card in self.cards.keys():
             if self.cards[card] == 5:
@@ -49,13 +48,61 @@ class Cardgame:
             if self.cards[card] == 2:
                 return True
         return False
+
+    def Classify(self):
+        if self.is5ofKind():
+            return 7
+        elif self.is4ofKind():
+            return 6
+        elif self.isFHouse():
+            return 5
+        elif self.is3ofKind():
+            return 4
+        elif self.is2Pair():
+            return 3
+        elif self.is1Pair():
+            return 2
+        else:
+            return 1 
+        
+
         
             
 def Part1(lines):
+    games = list()
     for line in lines:
-        game = Cardgame((line.split())[0])
+        #if game.is5ofKind():
+            #rank.append([game.cards, (line.split())[1], '5'])
+        g = line.split()
+        games.append([g[0], g[1], Cardgame(g[0]).Classify()])
     
+    games.sort(key=lambda x: x[2])
+    print(games)
+
     hierarchy = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+    unsolved = True
+    while(unsolved):
+        for i in range(0, len(games)): # Don't even try to figure this one out, but its basically insertion sort lol
+            unsolved = False
+            try:
+                if Cardgame(games[i][0]).Classify() == Cardgame(games[i+1][0]).Classify():           
+                    for j in range(0, len(games[i][0])):
+                        if hierarchy.index(games[i][0][j]) < hierarchy.index(games[i+1][0][j]):
+                            buffer = games[i]
+                            games[i] = games[i+1]
+                            games[i+1] = buffer
+                            #unsolved = True
+                            break
+            except:
+                continue
+    total = 0
+    for i in range(0, len(games)):
+        total += (i + 1) * int(games[i][1])
+    print(games)
+    return total
+    
+
+
 
 
 
